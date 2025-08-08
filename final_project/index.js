7aaa8099ea5802bf2ee99d554e55165c5fea6a8f
Kanishka Maduranga <madurangak@efuturesworld.com>
2025-08-08 13:13:21 +0530

    Customer Auth

--- final_project/index.js
+++ final_project/index.js
@@ -11,10 +11,28 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+
+    const authHeader = req.headers.authorization;
+    
+    if(!authHeader) return res.status(401).json({message : "Auth token not provided"});
+    const token = authHeader.split(' ')[1];
+
+    try{
+        jwt.verify(token, "access", (err, user) => {
+
+            if(!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({ message: "Customer not authenticated"});
+            }
+        });
+    } catch (err) {
+        return res.status(403).json({messge: "Customer not logged in"});
+    }
 });
  
-const PORT =5000;
+const PORT =5001;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
