c689243e11aedcf7e7dba5fc7c6d95e4f90e7f7d
Kanishka Maduranga <madurangak@efuturesworld.com>
2025-08-09 16:35:23 +0530

    getting book reviews from isbm

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -71,8 +71,16 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(!isbn) {
+    return res.status(400).json({message: "ISBN is required in URL"});
+  }
+
+  if(books[isbn]){
+    return res.status(200).json(books[isbn].reviews);
+  } else {
+    return res.status(404).json({message: "No books found for this isbn"});
+  }
 });
 
 module.exports.general = public_users;
